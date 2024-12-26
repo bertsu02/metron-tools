c4bb3af165dcd3e2c9cd72a954b24c033740542b
Bartek Zmuda <bertsu@protonmail.com>
2024-12-26 09:43:31 +0100

    wheel update

--- src/scripts/script-selector.js
+++ src/scripts/script-selector.js
@@ -1,41 +1,23 @@
-import clashWheelImg from "../img/clash-wheel.png";
 import bigWheelImg from "../img/big-wheel.png";
 import rainWheelImg from "../img/rain-wheel.png";
-import xWheelImg from "../img/x-wheel.png";
-import clashMarker from "../img/CLASH-MARKER.png";
 import bigMarker from "../img/BIG-MARKER.png";
 import rainMarker from "../img/RAIN-MARKER.png";
-import xMarker from "../img/x-marker.png";
-import clashButton from "../img/clash-button.png";
 import bigButton from "../img/big-button.png";
 import rainButton from "../img/rain-button.png";
-import xButton from "../img/x-button.png";
 
 (function () {
     const wheel = document.querySelector('.wheel');
     const startButton = document.querySelector('.button');
     const messageContainer = document.querySelector('.message-container');
 
-    const wheel1Image = document.getElementById("wheel1-img");
     const wheel2Image = document.getElementById("wheel2-img");
     const wheel3Image = document.getElementById("wheel3-img");
-    const wheel4Image = document.getElementById("wheel4-img");
     const marker = document.querySelector('.marker');
     const button = document.querySelector('.button');
 
     let deg = 0;
 
     const wheelConfigurations = {
-        wheel1: [
-            { name: '25 Gems Tip', startDeg: 0, endDeg: 9 },
-            { name: '5 Gems Tip', startDeg: 9, endDeg: 72 },
-            { name: '10 Gems Tip', startDeg: 72, endDeg: 135 },
-            { name: 'Battle Call', startDeg: 135, endDeg: 180 },
-            { name: '25 Gems Tip', startDeg: 180, endDeg: 189 },
-            { name: '5 Gems Tip', startDeg: 189, endDeg: 252 },
-            { name: '10 Gems Tip', startDeg: 252, endDeg: 315 },
-            { name: 'Battle Call', startDeg: 315, endDeg: 360 },
-        ],
         wheel2: [
             { name: '25 Coins Tip', startDeg: 0, endDeg: 9 },
             { name: '5 Coins Tip', startDeg: 9, endDeg: 72 },
@@ -56,19 +38,9 @@ import xButton from "../img/x-button.png";
             { name: '10 Coins Tip', startDeg: 252, endDeg: 315 },
             { name: 'Battle Call', startDeg: 315, endDeg: 360 },
         ],
-        wheel4: [
-            { name: '25 Coins Tip', startDeg: 0, endDeg: 9 },
-            { name: '5 Coins Tip', startDeg: 9, endDeg: 72 },
-            { name: '10 Coins Tip', startDeg: 72, endDeg: 135 },
-            { name: 'Battle Call', startDeg: 135, endDeg: 180 },
-            { name: '25 Coins Tip', startDeg: 180, endDeg: 189 },
-            { name: '5 Coins Tip', startDeg: 189, endDeg: 252 },
-            { name: '10 Coins Tip', startDeg: 252, endDeg: 315 },
-            { name: 'Battle Call', startDeg: 315, endDeg: 360 },
-        ],
     };
 
-    let currentWheel = 'wheel1';
+    let currentWheel = 'wheel2';
 
     function setWheel(wheelId, wheelImage) {
         currentWheel = wheelId;
@@ -76,37 +48,25 @@ import xButton from "../img/x-button.png";
         updateMarker(wheelId);
         updateButton(wheelId);
     }
+
     function updateMarker(wheelId) {
-        if (wheelId === 'wheel1') {
-            marker.src = clashMarker;
-        } else if (wheelId === 'wheel2') {
+        if (wheelId === 'wheel2') {
             marker.src = bigMarker;
         } else if (wheelId === 'wheel3') {
             marker.src = rainMarker;
-        }else if (wheelId === 'wheel4') {
-            marker.src = xMarker;
         }
     }
+
     function updateButton(wheelId) {
-        if (wheelId === 'wheel1') {
-            button.src = clashButton;
-        } else if (wheelId === 'wheel2') {
+        if (wheelId === 'wheel2') {
             button.src = bigButton;
         } else if (wheelId === 'wheel3') {
             button.src = rainButton;
-        }else if (wheelId === 'wheel4') {
-            button.src = xButton;
         }
     }
 
-    wheel1Image.src = clashWheelImg;
     wheel2Image.src = bigWheelImg;
     wheel3Image.src = rainWheelImg;
-    wheel4Image.src = xWheelImg;
-
-    wheel1Image.addEventListener('click', () => {
-        setWheel('wheel1', clashWheelImg);
-    });
 
     wheel2Image.addEventListener('click', () => {
         setWheel('wheel2', bigWheelImg);
@@ -115,9 +75,6 @@ import xButton from "../img/x-button.png";
     wheel3Image.addEventListener('click', () => {
         setWheel('wheel3', rainWheelImg);
     });
-    wheel4Image.addEventListener('click', () => {
-        setWheel('wheel4', xWheelImg);
-    })
 
     // Start button event listener
     startButton.addEventListener('click', () => {
