014160b23b4e461dc0ba9a61147dad97f3e33144
Bartek Zmuda <bertsu@protonmail.com>
2025-05-01 15:47:30 +0200

    fix3

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        src: resolve(__dirname, 'src/slots-wheel.html'),
-        spin: resolve(__dirname, 'src/sub-wheel.html'),
-        kev: resolve(__dirname, 'src/kevin-spin.html'),
-        deal: resolve(__dirname, 'src/deal.html'),
         plinko: resolve(__dirname, 'src/plinko.html'),
-        cards: resolve(__dirname, 'src/cards.html'),
-        matchcards:resolve(__dirname,'src/matchcards.html'),
-        callwheels:resolve(__dirname, 'src/call-wheels.html')
       },
     },
   },
